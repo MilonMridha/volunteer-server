ef3d0c1b81a6c52ac32882057b4879affa41246b
MilonMridha <milonmridha570@gmail.com>
2022-04-28 08:16:38 -0700

    added mongo

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
-const port = 5000 || process.env.PORT ;
+const port = process.env.PORT || 5000;
 const app = express();
 require('dotenv').config()
 //middleware 
